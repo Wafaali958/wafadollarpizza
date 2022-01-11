0b18ecfe52ab1101d72cf64fec9396bdfa52bf6d
Wafaali958 <wali1958@richmondhillhs.org>
2022-01-11 16:18:26 +0000

    some message describing changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import Plain from './plain';
-import Pepperoni from './pepperoni';
-import MeatLover from './meat-lover';
-import Veggie from './veggie';
 import './css/menu.css';
-
+import Slice from './Slice';
 function Menu() {
   return(
     <div className='Menu container-fluid'>
       <h1 className='primary-text'>New York's World Famous</h1>
       <h1 className='secondary-text'>Pizza Menu</h1>
       <div className='row'>
-        <Plain/>
-        <Pepperoni/>
-        <MeatLover/>
-        <Veggie/>
+        <Slice
+        img={plain}
+        title="Plain"
+        description="Its got cheese, its got sauce, and its only a dollar, What more would you ask for?"
+        price="1.00"/>
+       
+ 
+     <Slice 
+        img={pepperoni}
+        title="pepperoni"
+        description="Its got cheese, its got sauce, and its only a dollar, What more would you ask for?"
+        price="1.00"/>
 
+    <Slice 
+        img={hawaiian}
+        title="hawaiian"
+        description="Its got cheese, its got sauce, and its only a dollar, What more would you ask for?"
+        price="1.00"/>
+      
+      <Slice 
+        img={bbq}
+        title="bbq"
+        description="Its got cheese, its got sauce, and its only a dollar, What more would you ask for?"
+        price="1.00"/>
+      
+      
+      
       </div>
     </div>
   );
